d2b007da1d812972ec2d808c57c60a27500fffcc
NitinGupta <nitingupta@nitin-gupta.xebiaindia.local>
2017-05-16 10:43:24 +0530

    Util Functions Added.

--- Weather/Weather/Healpers/Constants.swift
+++ Weather/Weather/Healpers/Constants.swift
@@ -7,10 +7,16 @@
 //
 
 import Foundation
+import UIKit
 
 //Unit Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit
 enum TempratureUnitType : Int {
     case Kelvin = 0
     case Celsius = 1
     case Fahrenheit = 2
+}
+
+struct AppColor {
+    static let PrimaryColor = UIColor(colorLiteralRed: 0.0/255.0, green: 102/255.0, blue: 161/255.0, alpha: 1.0)
+    static let SecondaryColor = UIColor(colorLiteralRed: 181.0/255.0, green: 84/255.0, blue: 53/255.0, alpha: 1.0)
 }
--- Weather/Weather/Healpers/Utils.swift
+++ Weather/Weather/Healpers/Utils.swift
@@ -7,9 +7,15 @@
 //
 
 import Foundation
+import UIKit
 
 class Utils {
     
+    /// Delay Util Async Main thread util function
+    ///
+    /// - Parameters:
+    ///   - seconds: value in Sec
+    ///   - completion: completion block called after delay
     internal class func performWithDelay(seconds: Double, completion:@escaping ()->()) {
         let when = DispatchTime.now() + seconds
         DispatchQueue.main.asyncAfter(deadline: when) {
@@ -17,6 +23,10 @@ class Utils {
         }
     }
     
+    /// Getting String for Tempratature Type
+    ///
+    /// - Parameter type: Tempratature Unit Type
+    /// - Returns: String for Tempratature Type
     internal class func getTempratureUnitKey(forType type: TempratureUnitType) -> String {
         switch type {
         case .Celsius:
@@ -26,5 +36,38 @@ class Utils {
         case .Kelvin:
             return "Default"
         }
+    }
+    
+    /// Setting Up Navigation Controller Attributes
+    ///
+    /// - Parameter navigationController: navigationController instance
+    internal class func customizeNavigation(navigationController: UINavigationController) {
+        navigationController.navigationBar.barTintColor = AppColor.PrimaryColor
+        navigationController.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white, NSFontAttributeName: UIFont.systemFont(ofSize: 16.0)]
+        navigationController.navigationBar.isTranslucent = false
+        navigationController.navigationBar.tintColor = UIColor.white
+        navigationController.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController.navigationBar.shadowImage = UIImage()
+    }
+    
+    /// Setting Up Custom Application Statusbar
+    internal class func setUpCustomStatusbar() {
+        let aView = UIView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: 20))
+        aView.backgroundColor = AppColor.PrimaryColor
+        (UIApplication.shared.delegate as? AppDelegate)?.window?.rootViewController?.view .addSubview(aView)
+    }
+    
+    /// It simply Serialize JSON Data to [[String:Any]]
+    ///
+    /// - Parameter data: Url Response Data
+    /// - Returns: return value [[String:Any]]
+    internal class func serializeJSONData(data: Data) -> [[String:Any]]? {
+        do {
+            let jsonResult = try JSONSerialization.jsonObject(with: data, options:[]) as! [[String:Any]]
+            return jsonResult
+        } catch {
+            print("ERROR: Unable to Serialize Data to Dict")
+        }
+        return nil
     }
 }
